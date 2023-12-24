da5fd14c034aaf9f2a88acc9d6bf71dbc90a4b2c
Rohit4224 <rohit.khinchi1@gmail.com>
2023-12-24 10:55:17 +0100

    updated

---
+++ 01my_dir/35-Esercizio_Ospedale_facile/src/main/Medico.java
@@ -0,0 +1,18 @@
+package main;
+
+public class Medico
+{
+	  public String nome;
+	  public String cognome;
+	/**
+	 * @param nome
+	 * @param cognome
+	 */
+	public Medico(String nome, String cognome) {
+		this.nome = nome;
+		this.cognome = cognome;
+	}
+	  
+	  
+	}
+}
