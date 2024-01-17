75ca3b5d3b55d0d9ed475b2614c8cc24885f733e
Rohit4224 <rohit.khinchi1@gmail.com>
2024-01-17 09:24:16 +0100

    updated

--- 01my_dir/42 - Esercizio_Ristoratnti/src/main/Main.java
+++ 01my_dir/42 - Esercizio_Ristoratnti/src/main/Main.java
@@ -20,32 +20,32 @@ public class Main {
 		}
 		
 		
-		for (Piatto piatto : o.getPiatti()) {
-			System.out.println(piatto); //it implicitly call toString()
-		}
+//		for (Piatto piatto : o.getPiatti()) {
+//			System.out.println(piatto); //it implicitly call toString()
+//		}
 		
-		System.out.println(o.listaPiatti("acido"));
-		
-		System.out.println(o.piattiConIngrediente("zucchero"));
-		
-		String[] targetIngredients = {"zucchero", "olio"};
-		System.out.println(o.piattiConIngredienti(targetIngredients));
-		
+//		System.out.println(o.listaPiatti("acido"));
+//		
+//		System.out.println(o.piattiConIngrediente("zucchero"));
+//		
+//		String[] targetIngredients = {"zucchero", "olio"};
+//		System.out.println(o.piattiConIngredienti(targetIngredients));
+//		
 		System.out.println("\nl'oggetto Piatto più costoso: " + o.piuCostoso());
-		
-		System.out.println("\nil costo medio di tutti i piatti: " + o.costoMedioMenu());
-		
-		System.out.println("\ni piatti con valido()== false: " + o.piattiNonValidi());
-		
-		System.out.println("\nla moda del TIPO: \n" + o.tipoRicorrente());
-		
-		System.out.println("\nla moda dell'ingrediente: \n" + o.ingredienteRicorrente());
-		
-		System.out.println("\nse la moda del tipo è più di una, restituirle tutte sottoforma di arraylist: \n"
-								+ o.tipiRiccorenti());
-		
-		System.out.println("\nse la moda del ingrediente è più di una, restituirle tutte sottoforma di arraylist: \n"
-				+ o.ingredientiRicorrenti());
+//		
+//		System.out.println("\nil costo medio di tutti i piatti: " + o.costoMedioMenu());
+//		
+//		System.out.println("\ni piatti con valido()== false: " + o.piattiNonValidi());
+//		
+//		System.out.println("\nla moda del TIPO: \n" + o.tipoRicorrente());
+//		
+//		System.out.println("\nla moda dell'ingrediente: \n" + o.ingredienteRicorrente());
+//		
+//		System.out.println("\nse la moda del tipo è più di una, restituirle tutte sottoforma di arraylist: \n"
+//								+ o.tipiRiccorenti());
+//		
+//		System.out.println("\nse la moda del ingrediente è più di una, restituirle tutte sottoforma di arraylist: \n"
+//				+ o.ingredientiRicorrenti());
 		
 
 	}//main
